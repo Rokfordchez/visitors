895f5ff468947a8a7f38496e292f2e0f7d34637f
Rockfordchez <ilya.tleukenov@gmail.com>
2019-09-01 12:21:11 +0700

    syntax error

--- visitors/settings.py
+++ visitors/settings.py
@@ -22,6 +22,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
